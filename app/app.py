db640f75b18a445e2e160548f8151e4bd52e5aba
Alexander Booth <adbooth01@gmail.com>
2021-01-03 02:54:36 -0600

    update required

--- app/app.py
+++ app/app.py
@@ -93,5 +93,5 @@ def add_header(r):
 
 #main
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0")
-    # app.run(debug=True)
+    # app.run(debug=False, host="0.0.0.0")
+    app.run(debug=True)
